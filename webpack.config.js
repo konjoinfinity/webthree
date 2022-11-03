5bbda940a41cc8b89a5d25a47bdba6345b62b1f1
Wesley Scholl <clients@konjoweb.com>
2022-11-03 18:08:41 -0400

    finally fixed the polyfill issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+
 module.exports = {
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.resolve.alias.https = "https-browserify";
+      config.resolve.alias.http = "http-browserify";
+      return config;
     },
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   };

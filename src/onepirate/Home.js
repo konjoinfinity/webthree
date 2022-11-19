9845fb032394366fd2a3844eeb09d7e7ad2e6211
Wesley Scholl <clients@konjoweb.com>
2022-11-19 18:11:31 -0500

    added react lazy loading

--- src/onepirate/Home.js
+++ src/onepirate/Home.js
@@ -1,17 +1,19 @@
 import * as React from 'react';
-import ProductCategories from './modules/views/ProductCategories';
-import ProductSmokingHero from './modules/views/ProductSmokingHero';
-import AppFooter from './modules/views/AppFooter';
+import { Suspense } from 'react';
 import ProductHero from './modules/views/ProductHero';
-import ProductValues from './modules/views/ProductValues';
-import ProductHowItWorks from './modules/views/ProductHowItWorks';
-import ProductCTA from './modules/views/ProductCTA';
 import AppAppBar from './modules/views/AppAppBar';
 import withRoot from './modules/withRoot';
-import Animation from './modules/views/Animation.tsx';
 import Typography from './modules/components/Typography';
-import Specs from "./modules/views/Specs"
-import Footer from './modules/views/Footer';
+
+const ProductValues = React.lazy(() => import('./modules/views/ProductValues'));
+const Animation = React.lazy(() => import('./modules/views/Animation.tsx'));
+const Specs = React.lazy(() => import('./modules/views/Specs'));
+const ProductHowItWorks = React.lazy(() => import('./modules/views/ProductHowItWorks'));
+const ProductCategories = React.lazy(() => import('./modules/views/ProductCategories'));
+const ProductCTA = React.lazy(() => import('./modules/views/ProductCTA'));
+const ProductSmokingHero = React.lazy(() => import('./modules/views/ProductSmokingHero'));
+const AppFooter = React.lazy(() => import('./modules/views/AppFooter'));
+const Footer = React.lazy(() => import('./modules/views/Footer'));
 
 
 function Index() {
@@ -19,22 +21,40 @@ function Index() {
     <React.Fragment>
       <AppAppBar />
       <ProductHero />
+      <Suspense fallback={<div>Loading...</div>}>
       <ProductValues />
+      </Suspense>
       <div style={{backgroundColor: "#FEF7E9", padding: "5%"}}>
       <Typography variant="h4" marked="center" align="center" component="h2">
     Meet The Beauty Baebees
   </Typography>
   </div>
   <div style={{display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent:"center", alignItems:"center", marginBottom: "10vh"}}>
+  <Suspense fallback={<div>Loading...</div>}>
   <Animation />
+  </Suspense>
   </div>
+  <Suspense fallback={<div>Loading...</div>}>
   <Specs />
+  </Suspense>
+  <Suspense fallback={<div>Loading...</div>}>
       <ProductHowItWorks />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
       <ProductCategories />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
       <ProductCTA />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
       <ProductSmokingHero />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
       <AppFooter />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
       <Footer />
+      </Suspense>
     </React.Fragment>
   );
 }
